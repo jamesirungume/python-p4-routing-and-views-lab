ce77150ee47553ecc89bb24438a3c595b38e9aa4
Your Name <jymohcooler@gmail.com>
2023-09-18 15:09:04 +0300

    complete assignment

--- server/app.py
+++ server/app.py
@@ -4,5 +4,36 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
+
+@app.route('/print/<string:username>')
+def print_string(username):
+    print(username)
+    return username
+
+@app.route('/count/<int:num>')
+def count(num):
+    numbers = '\n'.join(map(str, range(num + 1)))  # Start from 0 and go up to num.
+    return numbers
+
+@app.route('/math/<int:num1>/<operation>/<int:num2>')
+def math(num1, operation, num2):
+    result = 0
+    if operation == '+':
+        result = num1 + num2
+    elif operation == '-':
+        result = num1 - num2
+    elif operation == '*':
+        result = num1 * num2
+    elif operation == 'div':
+        result = num1 / num2
+    elif operation == '%':
+        result = num1 % num2
+    return str(result)  # Convert the result back to a string.
+
+
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
